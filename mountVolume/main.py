b64ad08f7a286a775933bcfd5eb3e9b70a092a55
Yuki Iriyama <gajumaru19990110@gmail.com>
2022-06-23 13:37:50 +0900

    dbに接続できるようになりました

--- mountVolume/main.py
+++ mountVolume/main.py
@@ -2,5 +2,20 @@ import psycopg2
 import os
 
 def get_connection():
-    dsn = os.environ.get('postgresql://')
-    return psycopg2.connect(dsn)
+    connection = psycopg2.connect(
+        database = 'postgres',
+        user = 'postgres',
+        password='passw0rd',
+        host='0.0.0.0',
+        port = 5432
+    )
+    return connection
+
+conn = get_connection()
+cursor = conn.cursor()
+
+sql = """select * from weather;"""
+cursor.execute(sql)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
